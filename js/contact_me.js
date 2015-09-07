6822a5d65ff6a8b65de726cc507778aa56ab9f4a
AnvarTurobov <anvar.turobov@gmail.com>
2015-09-07 16:54:50 +0100

    got rid of php and js files for mail app

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -67,4 +67,4 @@ $(function() {
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
-});
+});*/
